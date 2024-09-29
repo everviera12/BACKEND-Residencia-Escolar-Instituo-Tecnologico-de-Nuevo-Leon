e78fbb96505fc71bae5b086319ce8d6a550f0d51
EverViera12 <evertigre10@gmail.com>
2024-09-28 18:44:37 -0600

    instalaciones mvc

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clientesRouter = require('./routes/clientes');
 var productosRouter = require('./routes/productos');
+var instalacionesRouter = require('./routes/instalaciones');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/clientes', clientesRouter);
 app.use('/productos', productosRouter);
+app.use('/instalciones', instalacionesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ src/model/instalacion.model.js
@@ -0,0 +1,81 @@
+const db = require("../../config/database");
+
+const getAllInstalaciones = async () => {
+  const result = await db.query("SELECT * FROM instalaciones");
+  return result.rows;
+};
+
+const getInstalacionById = async (id) => {
+  const result = await db.query("SELECT * FROM instalaciones WHERE id = $1", [
+    id,
+  ]);
+  return result.rows[0];
+};
+
+const addNewInstalacion = async (instalacionData) => {
+  const {
+    cliente_id,
+    producto_id,
+    fecha_instalacion,
+    descripcion,
+    costo_instalacion,
+    estado,
+  } = instalacionData;
+
+  const result = await db.query(
+    "INSERT INTO instalaciones (cliente_id, producto_id, fecha_instalacion, descripcion, costo_instalacion, estado) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [
+      cliente_id,
+      producto_id,
+      fecha_instalacion,
+      descripcion,
+      costo_instalacion,
+      estado,
+    ]
+  );
+
+  return result.rows[0];
+};
+
+const editInstalacion = async (instalacionData) => {
+  const {
+    id,
+    cliente_id,
+    producto_id,
+    fecha_instalacion,
+    descripcion,
+    costo_instalacion,
+    estado,
+  } = instalacionData;
+
+  const result = await db.query(
+    "UPDATE instalaciones SET cliente_id = $1, producto_id = $2, fecha_instalacion = $3, descripcion = $4, costo_instalacion = $5, estado = $6 WHERE id = $7 RETURNING *",
+    [
+      cliente_id,
+      producto_id,
+      fecha_instalacion,
+      descripcion,
+      costo_instalacion,
+      estado,
+      id,
+    ]
+  );
+
+  return result.rows[0];
+};
+
+const deleteInstalacion = async (id) => {
+  const result = await db.query(
+    "DELETE FROM instalaciones WHERE id = $1 RETURNING *",
+    [id]
+  );
+  return result.rows[0];
+};
+
+module.exports = {
+  getAllInstalaciones,
+  getInstalacionById,
+  addNewInstalacion,
+  editInstalacion,
+  deleteInstalacion,
+};
