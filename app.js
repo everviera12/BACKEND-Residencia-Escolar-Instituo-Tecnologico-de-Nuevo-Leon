03b2702acdbaccbe3f3fee349a6d0c32221d20e5
EverViera12 <evertigre10@gmail.com>
2024-09-29 15:29:33 -0600

    cors avilable

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var clientesRouter = require('./routes/clientes');
-var productosRouter = require('./routes/productos');
-var instalacionesRouter = require('./routes/instalaciones');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var clientesRouter = require("./routes/clientes");
+var productosRouter = require("./routes/productos");
+var instalacionesRouter = require("./routes/instalaciones");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, '/src/views'));
-app.set('view engine', 'pug');
+const allowedOrigins = ["http://localhost:3001", "http://localhost:3000"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("No permitido por CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Métodos permitidos
+    // allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "/src/views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/clientes', clientesRouter);
-app.use('/productos', productosRouter);
-app.use('/instalaciones', instalacionesRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/clientes", clientesRouter);
+app.use("/productos", productosRouter);
+app.use("/instalaciones", instalacionesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -37,11 +53,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
