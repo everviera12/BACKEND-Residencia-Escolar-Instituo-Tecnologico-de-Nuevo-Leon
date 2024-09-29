7cf60347f5094434ebf34dff9015210202d2b356
EverViera12 <evertigre10@gmail.com>
2024-09-29 14:12:10 -0600

    fix on instalaciones text

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/clientes', clientesRouter);
 app.use('/productos', productosRouter);
-app.use('/instalciones', instalacionesRouter);
+app.use('/instalaciones', instalacionesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
