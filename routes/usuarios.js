e7c4bf5e59778740628086522387d9639aa25d48
EverViera12 <evertigre10@gmail.com>
2024-10-02 21:04:27 -0600

    post rout and auth cookie

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const usuariosController = require("../src/controller/usuarios.controller");
+const checkAdminCookie = require("../config/middleware/auth");
+
+// validamos la cookie
+router.use(checkAdminCookie);
+
 
 router.get("/", usuariosController.getAllUsers);
 router.get("/usuario/:id", usuariosController.getUserById);
+router.post("/usuario", usuariosController.createUser);
 router.delete("/usuario/:id", usuariosController.deleteUser);
+
+
+// router.post("/login", usuariosController.login); 
 
 module.exports = router;
