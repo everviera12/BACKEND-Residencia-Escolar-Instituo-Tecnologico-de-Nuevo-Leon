ca2b2c0829c80f6a55d235d40084b6f6d53eada5
EverViera12 <evertigre10@gmail.com>
2024-10-02 21:04:04 -0600

    login route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const usuariosController = require("../src/controller/usuarios.controller");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post("/login", usuariosController.login); 
+
 
 module.exports = router;
